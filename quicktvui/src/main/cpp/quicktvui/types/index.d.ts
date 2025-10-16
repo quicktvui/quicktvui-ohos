fe54d559161c3c316c0c30ea174769f8b1b612fb
zhaopengdev <zhaopengdev@163.com>
2025-10-16 14:50:45 +0800

    feat: 1. 项目结构调整，依赖hippy_ohos_tv及hippy_ohos_tv_extend俩个项目。2. 合并feat-quikctvui-ohos最新分支代码

--- quicktvui/src/main/cpp/quicktvui/types/index.d.ts
+++ quicktvui/src/main/cpp/quicktvui/types/index.d.ts
@@ -1,56 +1,56 @@
-  export interface  QTNativeLayout {
-    constructor();
+export interface  QTNativeLayout {
+  constructor();
 
-    GetWidth(): number;
-    GetHeight(): number;
-    GetLeft(): number;
-    GetTop(): number;
-    GetRight(): number;
-    GetBottom(): number;
-    GetStyleWidth(): number;
-    GetStyleHeight(): number;
+  GetWidth(): number;
+  GetHeight(): number;
+  GetLeft(): number;
+  GetTop(): number;
+  GetRight(): number;
+  GetBottom(): number;
+  GetStyleWidth(): number;
+  GetStyleHeight(): number;
 
-    SetWidth(value: number): void;
-    SetHeight(value: number): void;
-    SetMaxWidth(value: number): void;
-    SetMaxHeight(value: number): void;
-    SetScaleFactor(value: number): void;
-    SetPosition(edge: Edge, position: number): void;
+  SetWidth(value: number): void;
+  SetHeight(value: number): void;
+  SetMaxWidth(value: number): void;
+  SetMaxHeight(value: number): void;
+  SetScaleFactor(value: number): void;
+  SetPosition(edge: Edge, position: number): void;
 
-    HasNewLayout(): boolean;
-    IsDirty(): boolean;
-    MarkDirty(): void;
-    CalculateLayout(width: number, height: number): void;
+  HasNewLayout(): boolean;
+  IsDirty(): boolean;
+  MarkDirty(): void;
+  CalculateLayout(width: number, height: number): void;
 
-    InsertChild(child: QTNativeLayout, index: number): void;
-    RemoveChild(child: QTNativeLayout): void;
+  InsertChild(child: QTNativeLayout, index: number): void;
+  RemoveChild(child: QTNativeLayout): void;
 
-    ResetLayoutCache(): void;
-  }
+  ResetLayoutCache(): void;
+}
 
-  export enum Edge {
-    EdgeLeft,
-    EdgeTop,
-    EdgeRight,
-    EdgeBottom,
-    EdgeStart,
-    EdgeEnd
-  }
+export enum Edge {
+  EdgeLeft,
+  EdgeTop,
+  EdgeRight,
+  EdgeBottom,
+  EdgeStart,
+  EdgeEnd
+}
 
-  export enum Direction {
-    Inherit,
-    LTR,
-    RTL
-  }
+export enum Direction {
+  Inherit,
+  LTR,
+  RTL
+}
 
-  export enum LayoutMeasureMode {
-    Undefined,
-    Exactly,
-    AtMost
-  }
+export enum LayoutMeasureMode {
+  Undefined,
+  Exactly,
+  AtMost
+}
 
-  export enum LayoutEngineType {
-    LayoutEngineDefault,
-    LayoutEngineTaitank,
-    LayoutEngineYoga
-  }
+export enum LayoutEngineType {
+  LayoutEngineDefault,
+  LayoutEngineTaitank,
+  LayoutEngineYoga
+}
