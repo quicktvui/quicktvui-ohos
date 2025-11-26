d8366273b18658c8a1ea8e94a1bc1f400a0f293d
weipeng <weipeng@huan.tv>
2025-11-26 18:24:17 +0800

    feat(hippy): 从assets加载应用

--- scripts/test-eskit.sh
+++ scripts/test-eskit.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-hdc shell "aa start -b com.extscreen.runtime -a RuntimeEntryAbility --ps pkg es.hello.world --ps url 'http://192.168.7.7/pub/ohos/test.zip'"
+hdc shell "aa start -b com.extscreen.runtime -a RuntimeEntryAbility --ps pkg es.hello.world --ps uri 'http://192.168.7.7/pub/ohos/test.zip'"
 
 
 #hdc shell "aa start -b com.extscreen.runtime -a RuntimeEntryAbility --ps uri 'esapp://es.hello.world?url=http://192.168.7.7/pub/ohos/test.zip'"
