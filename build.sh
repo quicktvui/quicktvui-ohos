584af5a8195a5630c3a5d01292bbfc1f77aa343d
liulipeng <pengliliu8086@163.com>
2025-12-29 15:40:49 +0800

    多渠道打包

--- build.sh
+++ build.sh
@@ -71,7 +71,16 @@ build_channel() {
 # 4. 渠道列表
 # ========================================================
 CHANNELS=(
-app_center
+ctyangshenggong
+haoqidongwu
+huohuotu
+konglongxingqiu
+guangchangwu
+shoushijin
+xingainian
+hsport
+
+#app_center
 #ke_ting_jian_shen
 #tudoudou
 #baduanjin
